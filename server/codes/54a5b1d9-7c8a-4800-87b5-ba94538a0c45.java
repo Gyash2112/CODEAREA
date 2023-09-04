51c28f36a0dbec3d4f6b71395d3f19554e60c6b0
unknown <gyash.21dec@gmail.com>
2023-09-04 22:49:01 +0530

    added server

---
+++ server/codes/54a5b1d9-7c8a-4800-87b5-ba94538a0c45.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+class Main{
+  public static void main(String args[]){
+    //Write your code here
+    System.out.println("YASH");
+    Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a+b);
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import { useRef } from 'react';
 import Editor from '@monaco-editor/react';
+import './app.css';
+import axios from 'axios';
+import { ClapSpinner } from 'react-spinners-kit';
 const Footer = () => {
 
+  
+
   let data = {
+    themes:["vs-dark","vs-light"],
     languages: ["java", "cpp", "python"],
+
 
     langDetails: [
       {
@@ -48,52 +55,108 @@ print(fun())
   }
 
   const editorRef = useRef(null);
+  
 
-
+  const [input, setInput]= useState("");
   const [lang, setLang] = useState("java")
+
+  const [theme,setTheme] =useState("vs-dark");
+  const [output,setOutput]=useState("");
+  const [loading,setLoading] = useState(false);
 
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
+    console.log(monaco.languages);
   }
 
-  function showValue() {
-    alert(editorRef.current.getValue());
+ function showValue() {
+   
+  setLoading(true);
+  axios.post(`http://localhost:8000/compile`, {
+    code: editorRef.current.getValue(),
+    language: lang,
+    input:input
+
+  }).then((res)=>{
+    console.log(res);
+    // alert(res.data.output)
+    setOutput(res.data)
+    setOutput(res.data.output);
+    // setInput("");
+  }).then(()=>{setLoading(false)})
+  
   }
   const handleSelection = (event) => {
-    setLang(event.target.value);
+    setLang(event.target.value)
+    // if(event.target.value === "python"){
+      
+    // }
+    
 
   }
+
+  const handleTheme = (event)=>{
+    setTheme(event.target.value);
+  }
+
+  
   // console.log(lang, "sla")
 
 
   return (
     <>
     {console.log(data.langDetails.find(element => element.defaultLanguage === lang).content,"chekcsai")}
-
-    
-      
-
-      <select onChange={handleSelection} >
+    <div className='com_head'>
+      <div className='languages'>
+      <select onChange={handleSelection} className='language'>
         {data.languages.map((element) =>
           <option value={`${element}`}> {element}</option>
         )}
+        </select>
 
+        <select onChange={handleTheme} className='language'>
+        {data.themes.map((element) =>
+          <option value={`${element}`}> {element}</option>
+        )}
+        </select>
+      </div>
+      <div className='switch'>
+        <img alt='theme' src='https://cdn-icons-png.flaticon.com/128/5043/5043139.png'/>
+        <button onClick={showValue} className='submit'>RUN</button>
+      </div>
+    </div>
+    
+      
 
-      </select>
-      <button onClick={showValue}>Click</button>
+      
+
+      <div className='compiler'>
       <Editor
         height="90vh"
+        width="60%"
+        options={{fontSize:"15px"}}
         path={`${data.langDetails.find(element => element.defaultLanguage === lang).id}`}
         defaultLanguage={`${data.langDetails.find(element => element.defaultLanguage === lang).defaultLanguage}`}
         defaultValue={`${data.langDetails.find(element => element.defaultLanguage === lang).content}`}
         onMount={handleEditorDidMount}
-        theme='vs-dark'
+        loading={<ClapSpinner/>}
+        theme={`${data.themes.find(element => element === theme)}`}
       />
 
-          {console.log("hey")}
 
+        <div className='right-side'>
+          <div className="input-box">
+            <h3>INPUT</h3>
+            <textarea rows="20" cols="20" onChange={(e)=>{setInput(e.target.value)}}></textarea>
+          </div>
 
-
+          <div className='input-box'>
+            <h3>OUTPUT</h3>
+            {loading?<ClapSpinner size={30} loading={loading}/>:<textarea readOnly={true} rows="20" cols="20" value={output}></textarea>}
+            
+          </div>
+        </div>
+      </div>
     </>
   )
 }
